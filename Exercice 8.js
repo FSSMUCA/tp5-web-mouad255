bbcc4360659a0d14d785db7b66335838142026bb
MOUAD BAHAIDA <m.bahaida1482@uca.ac.ma>
2025-12-11 23:18:15 +0100

    Add files via upload

---
+++ Exercice 8.js
@@ -0,0 +1,11 @@
+function totalAvecRemise(total, remise) {
+  let totalFinal = total - (total * remise / 100);
+  return totalFinal;
+}
+
+let totalHT = Number(prompt("Entrez le total HT : "));
+let remise = Number(prompt("Entrez la remise en % : "));
+
+
+let totalFinal = totalAvecRemise(totalHT, remise);
+console.log("Total après remise :", totalFinal);
